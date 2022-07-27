de29eed66215dd32e63975fcb70e8c49984894f6
P-cans <107673716+P-cans@users.noreply.github.com>
2022-07-27 18:02:47 +0900

    메모1.02_쓰기, 반복해서 실행

--- 메모/memo.py
+++ 메모/memo.py
@@ -1,22 +1,25 @@
-print("선택해")
-print("1. 메모작성 2. 메모읽기")
+while True:
+    print("기능을 선택해주세요.")
+    print("1. 메모작성 2. 메모읽기 3.종료하기")
 
-option = input()
+    option = input()
 
-print(option)
+    print(option)
 
-if option == '1':
-    newmemo = input("메모를 입력해주세요 \n")
-    print(newmemo)
-    filename = 'memo.txt'
-    f = open(filename, 'a')
-    f.write(newmemo)
-    f.close()
-elif option == '2':
-    file_name = 'memo2.txt'
-    f = open(file_name)
-    memo = f.read()
-    print(memo)
-    f.close()
-else:
-    print("잘못됨.")
+    if option == '1':
+        newmemo = input("메모를 입력해주세요 \n")
+        print(newmemo)
+        filename = 'memo.txt'
+        f = open(filename, 'a')
+        f.write(newmemo)
+        f.close()
+    elif option == '2':
+        file_name = 'memo2.txt'
+        f = open(file_name)
+        memo = f.read()
+        print(memo)
+        f.close()
+    elif option =='3':
+        break
+    else:
+        print("잘못됨.")
