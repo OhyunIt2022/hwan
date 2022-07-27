aa77dfb6349ef889e1d5d8c022d3f88003849887
P-cans <107673716+P-cans@users.noreply.github.com>
2022-07-27 17:56:16 +0900

    메모장만들기1.01

--- 메모/memo.py
+++ 메모/memo.py
@@ -6,10 +6,17 @@ option = input()
 print(option)
 
 if option == '1':
-    print("아직 구현 안함")
+    newmemo = input("메모를 입력해주세요 \n")
+    print(newmemo)
+    filename = 'memo.txt'
+    f = open(filename, 'a')
+    f.write(newmemo)
+    f.close()
 elif option == '2':
-    f = open('memo2.txt')
+    file_name = 'memo2.txt'
+    f = open(file_name)
     memo = f.read()
     print(memo)
+    f.close()
 else:
     print("잘못됨.")
